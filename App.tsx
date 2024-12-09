c7befd9247caa7aaecb4af142cea6954a4814f6c
Sharandeep Singh <129933469+Sharan-deepSingh@users.noreply.github.com>
2024-12-09 14:12:31 +0530

    Flat List Basic Implementation

--- App.tsx
+++ App.tsx
@@ -1,118 +1,101 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import { useState } from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
+  Button,
+  FlatList,
   StyleSheet,
   Text,
-  useColorScheme,
+  TextInput,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+function App() {
+  const [name, setName] = useState("")
+  const [password, setPassword] = useState("")
+  const [email, setEmail] = useState("")
+  const [display, setDisplay] = useState(false)
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  const setEmptyState = () => {
+    setName("")
+    setPassword("")
+    setEmail("")
+    setDisplay(false)
+  }
 
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const flatListData = [
+  {
+    name: "Mr. Singh",
+    age: 16
+  },
+  {
+    name: "Mr. ffsf",
+    age: 16
+  },
+  {
+    name: "fsdsdffds",
+    age: 16
+  }
+  ]
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View>
+      <TextInput 
+      style={style.textInput}
+      placeholder='Enter Your Name'
+      value={name}
+      onChangeText={(text)=>setName(text)}
+      />
+      <TextInput 
+      style={style.textInput}
+      placeholder='Enter Your Password'
+      value={password}
+      secureTextEntry={true}
+      onChangeText={(text)=>setPassword(text)}
+      />
+      <TextInput 
+      style={style.textInput}
+      placeholder='Enter Your Email'
+      value={email}
+      onChangeText={(text)=>setEmail(text)}
+      />
+      <View style={style.buttonStyle}>
+        <Button color='white' title='Press Me to Display' onPress={()=>setDisplay(true)} />
+      </View>
+      <View style={style.buttonStyle}>
+        <Button color='white' title='Press Me to remove' onPress={()=>setEmptyState()} />
+      </View>
+      <FlatList 
+        data={flatListData}
+        renderItem={({item})=><Text>{item.name}</Text>}
+      />
+      {
+        display ? 
+        <View>
+          <Text>Name is: {name}</Text>
+          <Text>Password is: {password}</Text>
+          <Text>Email is is: {email}</Text>
+        </View>
+        : null
+      }
+
     </View>
   );
 }
-
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+const style = StyleSheet.create({
+  textInput: {
+     color: 'gray',
+     borderColor: 'black',
+     borderWidth: 2,
+     fontSize: 20,
+     borderRadius: 8,
+     marginTop: 50,
+     marginLeft: 10,
+     marginRight: 40,
+     height: 40
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
+  buttonStyle: {
+    backgroundColor: 'pink',
+    borderRadius: 8,
+    margin: 20
+  }
+})
 export default App;
