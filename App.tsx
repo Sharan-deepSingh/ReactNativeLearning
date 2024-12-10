4ff8cb66c181f690e964775697f36d94053ef82d
Sharandeep Singh <129933469+Sharan-deepSingh@users.noreply.github.com>
2024-12-10 10:31:51 +0530

    Made a grid layout using map method and custom View

--- App.tsx
+++ App.tsx
@@ -1,118 +1,93 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
-  SafeAreaView,
   ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+function App() {
+  const gridData = [
+    {
+      name: "Mr. Singh",
+      id: 1
+    },
+    {
+      name: "Parneet",
+      id: 2
+    },
+    {
+      name: "Shubam",
+      id: 3
+    },
+    {
+      name: "Rahul",
+      id: 4
+    },
+    {
+      name: "Yogesh",
+      id: 5
+    },
+    {
+      name: "Jaspinder",
+      id: 6
+    },
+    {
+      name: "Karan",
+      id: 7
+    },
+    {
+      name: "Varinder",
+      id: 8
+    },
+    {
+      name: "Harwinder",
+      id: 9
+    },
+    {
+      name: "Harwinder",
+      id: 10
+    }
+  ]
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View>
+      <Text style={styles.textStyle}>Grid Demo</Text>
+
+      <ScrollView>
+        <View style={styles.outerView}>
+            {
+              gridData.map((data) => 
+                <View style={styles.boxStyle} key={data.id}>
+                  <Text>{data.name}</Text>
+                </View>
+              )
+            }
+        </View>
+      </ScrollView>
     </View>
   );
 }
 
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  textStyle: {
+    marginTop: 40,
+    fontSize: 20,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+  boxStyle: {
+    backgroundColor: 'red',
+    width: 115,
+    height: 115,
+    padding: 5,
+    margin: 5,
+    alignItems: 'center', 
+    justifyContent: 'center',
+  }, 
+  outerView: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    height: 600 
+  }
+}) 
 
 export default App;
