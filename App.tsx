2d5a49becc7c3b1e56ebefbb75b90db6b7e9e6f5
Sharandeep Singh <129933469+Sharan-deepSingh@users.noreply.github.com>
2024-12-18 16:55:00 +0530

    Implemented useEffect, useState hooks

--- App.tsx
+++ App.tsx
@@ -1,118 +1,54 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, Button, TouchableOpacity } from 'react-native';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+function View2(props) {
+  const [v2, setv2] = useState(0)
+  useEffect(()=> {
+    setv2((oldV2)=>oldV2+1)
+  },
+  [props.value])
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View>
+      <Text>This is view 2 we will change value from here {v2}</Text>
+      <Button title='a good button' onPress={() => props.setValue(()=>10)}/>
     </View>
-  );
+  )
 }
 
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+function App() {
+  const [value, setValue] = useState(0)
+  const [value2, setValue2] = useState(0)
+  useEffect(() => {
+    setValue2(value + 1)
+    console.warn(`I am use effect ${value}`)
+  }, [value])
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+  return(
+    <View>
+      <Text style={styles.textStyle}>Hello World {}</Text>
+      <TouchableOpacity style={styles.buttonStyle} onPress={()=>setValue((oldValue)=>oldValue+1)}>
+        <Text>Press Me</Text>
+      </TouchableOpacity>
+      <Text style={styles.textStyle}>Hello World Value 2 = {}</Text>
+      <View2 value={value} />
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+  textStyle : {
+    marginTop: 70, 
+    fontSize: 25,
+    marginLeft: 20,
+  }, 
+  buttonStyle: {
+    width: 170,
+    height: 50,
+    backgroundColor: 'green',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
 
 export default App;
