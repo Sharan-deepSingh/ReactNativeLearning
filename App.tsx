9824d0518f5560ed46df9a7b1fbd27b21a933618
Sharandeep Singh <129933469+Sharan-deepSingh@users.noreply.github.com>
2025-01-07 13:23:53 +0530

    Implemented various UI Components
    Implemented
    
    - Status bar styling for iOS
    - Activity Indicator
    - Pressable
    - Modal

--- App.tsx
+++ App.tsx
@@ -1,5 +1,11 @@
 import { useState } from 'react';
 import {
+  _View,
+  ActivityIndicator,
+  Button,
+  Modal,
+  Pressable,
+  StatusBar,
   StyleSheet,
   Text,
   TouchableOpacity,
@@ -8,79 +14,92 @@ import {
 
 
 function App() {
-  const [selectedId, setSelectedId] = useState(1)
-  const data = [
-    {
-      language: 'Python',
-      id: 1
-    },
-    {
-      language: 'Swift',
-      id: 2
-    },
-    {
-      language: 'Java',
-      id: 3
-    },
-    {
-      language: 'java Script',
-      id: 4
-    },
-    {
-      language: 'C++',
-      id: 5
-    },
-  ]
+  const [animating, setAnimating] = useState(false)
+  const [modalPresented, setModalPresented] = useState(false)
 
   return (
-    <View style={styles.mainView}>
-      <View>
-      {
-        data.map((item) => (
-          <TouchableOpacity style={styles.button} onPress={() => setSelectedId(item.id)}>
-            <View style={styles.radio}>
-              { item.id == selectedId ? <View style={styles.buttonSelector} ></View> : null}
-            </View>
-            <Text style={styles.textStyle}>{item.language}</Text>
-          </TouchableOpacity>
-        ))
-      }
+    // iOS don't support background color so we need to use a view give height to it as required and give backgroudn color to it as done in below example
+    <View style={{ flex: 1 }}>
+      <View style={{ height: 50, backgroundColor: 'green' }}>
+        <StatusBar
+          animated={true}
+          // backgroundColor={'green'} iOS don't support this
+          barStyle={'dark-content'}
+          translucent={false}
+          showHideTransition={'slide'}
+        />
       </View>
+
+      <Text style={styles.textStyle}>This is an example for Activity Indecator, Modal, Status Bar and Pressable</Text>
+      <View style={styles.activityIndicatorView}>
+        <ActivityIndicator />
+        <ActivityIndicator animating={animating} size={'large'} />
+        <ActivityIndicator color='green' />
+        <ActivityIndicator color='purple' size='large' />
+        <Button title='Press me to see magic' onPress={() => setAnimating(true)} />
+      </View>
+
+      <View style={styles.modalMainView} >
+        <Pressable
+          onPressOut={() => setModalPresented(true)}
+        >
+          <Text>Press Me to see Modal and how (onPressOut works) in Pressable</Text>
+        </Pressable>
+
+        <Modal
+          animationType='slide'
+          visible={modalPresented}
+          transparent={true}
+        >
+          <View style={styles.modalTopLevelView}>
+            <View style={styles.modalContent}>
+              <Text>This is a simple modal and Pressable example</Text>
+              <Pressable
+                onLongPress={() => setModalPresented(false)}
+                style={{ borderRadius: 8, backgroundColor: 'red', marginTop: 20 }}
+              >
+                <Text style={styles.longPressTextStyle}>Long press to close Modal</Text>
+              </Pressable>
+            </View>
+          </View>
+        </Modal>
+      </View>
+
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create(
   {
-    mainView: {
+    textStyle: {
+      fontSize: 14,
+      marginTop: 70,
+      margin: 20
+    },
+    activityIndicatorView: {
       backgroundColor: 'lightpink',
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center'
+      margin: 20,
+      padding: 20
     },
-    button: {
-      flexDirection: 'row',
-      margin: 10,
-    },
-    radio: {
-      borderColor: 'black',
-      borderWidth: 2,
-      width: 20,
-      height: 20,
-      borderRadius: 16,
+    modalMainView: {
       justifyContent: 'center',
       alignItems: 'center'
     },
-    buttonSelector: {
-      backgroundColor: 'green',
-      width: 12,
-      height: 12,
+    modalTopLevelView: {
+      backgroundColor: "rgba(0,0,0,0.5)",
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    modalContent: {
+      backgroundColor: 'white',
+      padding: 20,
       borderRadius: 16
     },
-    textStyle: {
-      marginLeft: 10,
-      fontWeight: 'bold',
-      fontSize: 16,
+    longPressTextStyle: {
+      color: 'white',
+      textAlign: 'center',
+      padding: 6
     }
   }
 )
