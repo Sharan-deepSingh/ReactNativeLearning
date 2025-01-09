31629108b015ddcb966dd80edcfb705c929c8057
Sharandeep Singh <129933469+Sharan-deepSingh@users.noreply.github.com>
2025-01-09 12:43:49 +0530

    Implemented webview and used Platform module of react-native

--- App.tsx
+++ App.tsx
@@ -1,105 +1,56 @@
 import { useState } from 'react';
 import {
-  _View,
-  ActivityIndicator,
-  Button,
-  Modal,
-  Pressable,
-  StatusBar,
   StyleSheet,
   Text,
-  TouchableOpacity,
   View,
+  Platform
 } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 
 function App() {
-  const [animating, setAnimating] = useState(false)
-  const [modalPresented, setModalPresented] = useState(false)
-
   return (
-    // iOS don't support background color so we need to use a view give height to it as required and give backgroudn color to it as done in below example
     <View style={{ flex: 1 }}>
-      <View style={{ height: 50, backgroundColor: 'green' }}>
-        <StatusBar
-          animated={true}
-          // backgroundColor={'green'} iOS don't support this
-          barStyle={'dark-content'}
-          translucent={false}
-          showHideTransition={'slide'}
+      <View style={styles.webView}>
+        <WebView
+          source={{ uri: "https://softprodigy.com/" }}
         />
       </View>
 
-      <Text style={styles.textStyle}>This is an example for Activity Indecator, Modal, Status Bar and Pressable</Text>
-      <View style={styles.activityIndicatorView}>
-        <ActivityIndicator />
-        <ActivityIndicator animating={animating} size={'large'} />
-        <ActivityIndicator color='green' />
-        <ActivityIndicator color='purple' size='large' />
-        <Button title='Press me to see magic' onPress={() => setAnimating(true)} />
+      <View style={styles.textArea}>
+        <Text style={styles.heading}>Platform Information</Text>
+        <Text style={styles.bodyText}>You are currently using {Platform.OS} platform</Text>
       </View>
-
-      <View style={styles.modalMainView} >
-        <Pressable
-          onPressOut={() => setModalPresented(true)}
-        >
-          <Text>Press Me to see Modal and how (onPressOut works) in Pressable</Text>
-        </Pressable>
-
-        <Modal
-          animationType='slide'
-          visible={modalPresented}
-          transparent={true}
-        >
-          <View style={styles.modalTopLevelView}>
-            <View style={styles.modalContent}>
-              <Text>This is a simple modal and Pressable example</Text>
-              <Pressable
-                onLongPress={() => setModalPresented(false)}
-                style={{ borderRadius: 8, backgroundColor: 'red', marginTop: 20 }}
-              >
-                <Text style={styles.longPressTextStyle}>Long press to close Modal</Text>
-              </Pressable>
-            </View>
-          </View>
-        </Modal>
-      </View>
-
     </View>
   )
 }
 
 const styles = StyleSheet.create(
   {
-    textStyle: {
-      fontSize: 14,
-      marginTop: 70,
+    webView: {
+      marginTop: 60,
+      flex: 7
+    },
+    textArea: {
+      flex: 1,
+      marginBottom: 30,
+      marginLeft: 30,
+      marginRight: 30,
+      backgroundColor: 'rgba(0,0,0,0.1)',
+      borderWidth: 4,
+      borderTopLeftRadius: 36,
+      borderBottomRightRadius: 36,
+      borderStyle: 'dashed'
+    },
+    heading: {
+      fontSize: 20,
+      fontWeight: 'bold',
       margin: 20
     },
-    activityIndicatorView: {
-      backgroundColor: 'lightpink',
-      margin: 20,
-      padding: 20
-    },
-    modalMainView: {
-      justifyContent: 'center',
-      alignItems: 'center'
-    },
-    modalTopLevelView: {
-      backgroundColor: "rgba(0,0,0,0.5)",
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center'
-    },
-    modalContent: {
-      backgroundColor: 'white',
-      padding: 20,
-      borderRadius: 16
-    },
-    longPressTextStyle: {
-      color: 'white',
-      textAlign: 'center',
-      padding: 6
+    bodyText: {
+      fontSize: 16,
+      marginLeft: 20,
+      fontStyle: 'italic'
     }
   }
 )
