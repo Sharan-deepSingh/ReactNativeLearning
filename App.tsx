b442fcc06a792788a64c33f488fd500c0338ece7
Sharandeep Singh <129933469+Sharan-deepSingh@users.noreply.github.com>
2025-01-14 16:31:53 +0530

    Implemented bottom tab navigation

--- App.tsx
+++ App.tsx
@@ -2,19 +2,19 @@ import { useState } from 'react';
 import { View, Button, SafeAreaView } from 'react-native';
 import Login from './LoginScreens/Login';
 import {NavigationContainer} from '@react-navigation/native'
-import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs'
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
 import Signup from './LoginScreens/Signup';
 
 const App = () => {
-  const TopTab = createMaterialTopTabNavigator();
+  const bottomTab = createBottomTabNavigator();
 
   return (
     <NavigationContainer>
       <SafeAreaView style={{ flex: 1 }}>
-        <TopTab.Navigator>
-          <TopTab.Screen name="Login" component={Login} />
-          <TopTab.Screen name="Signup" component={Signup} />
-        </TopTab.Navigator>
+        <bottomTab.Navigator>
+          <bottomTab.Screen name="Login" component={Login} />
+          <bottomTab.Screen name="Signup" component={Signup} />
+        </bottomTab.Navigator>
       </SafeAreaView>
     </NavigationContainer>
   );
