a80b10fec636a019b7376b1dd2b9d2fbbecfefe3
Sharandeep Singh <129933469+Sharan-deepSingh@users.noreply.github.com>
2025-01-21 10:30:32 +0530

    Implemented post api with form

--- App.tsx
+++ App.tsx
@@ -1,40 +1,79 @@
 import { useEffect, useState } from 'react';
 import {
   FlatList,
+  StyleSheet,
   Text,
+  TextInput,
+  TouchableOpacity,
   View
 } from 'react-native';
 
 function App() {
-  const [data, setData] = useState(null)
+  const [id, setId] = useState(null)
+  const [age, setAge] = useState(null)
+  const [name, setName] = useState(null)
 
-  const getData = async () => {
-    const url = "http://192.168.5.253:4000/users"
-    const fetchResult = await fetch(url)
-    const result = await fetchResult.json()
-    setData(result)
-    console.warn(result)
+  const saveData = async (data) => {
+    const url = "http://192.168.5.253:3000/users"
+    const result = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      "body": JSON.stringify(data)
+    })
   }
 
-  useEffect(()=>{
-    getData()
-  }, [])
-
   return (
-    <View>
-      <FlatList 
-        data={data}
-        renderItem={({item}) => 
-          <View>
-            <Text>{item.id}</Text>
-            <Text>{item.name}</Text>
-            <Text>{item.age}</Text>
-          </View>
+    <View style={styles.mainView}>
+      <CTextField placeholder="Please Enter User Id" setValue={setId}/>
+      <CTextField placeholder="Please Enter your age" setValue={setAge}/>
+      <CTextField placeholder="Please Enter your name" setValue={setName}/>
+      <TouchableOpacity onPress={() => saveData({
+        age,
+        name
+      })} style={styles.button}>
+        <Text style={styles.text}>Press to save</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
 
-        }
+function CTextField(props) {
+  return(
+    <View style={styles.textField}>
+      <TextInput 
+        placeholder={props.placeholder}
+        onChangeText={(text) => props.setValue(text)}
       />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  mainView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  textField: {
+    borderWidth: 2,
+    borderColor: 'black',
+    borderRadius: 8,
+    padding: 10,
+    width: 300,
+    margin: 10
+  },
+  button: {
+    padding: 4,
+    width: 150,
+    backgroundColor: 'green',
+    borderRadius: 8,
+    alignItems: 'center'
+  },
+  text: {
+    color: 'white'
+  }
+})
 
 export default App;
